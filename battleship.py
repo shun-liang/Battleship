c0c180f9fb681453e903d9a6034121cca38fa9b9
Shun Liang <shun.liang.public@gmail.com>
2016-01-18 20:05:26 +0000

    Added ocean data structure,

--- battleship.py
+++ battleship.py
@@ -1,9 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+ocean = [[False for x in range(10)] for x in range(10)]
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "The ocean: %s" % ocean
 
 if __name__ == "__main__":
+    #print ocean[0][0]
     app.run()
